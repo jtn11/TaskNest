7851c736d52c8f8e209ced90ef5419133bf19d37
Phoenix <Phoenix@192.168.1.3>
2025-07-06 20:05:12 +0530

    changed structure of routing navItems

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,10 +3,6 @@ import { ReactNode, useState } from "react";
 import { SideBar } from "../components/sidebar";
 import { NavBar } from "../components/navbar";
 
-
-type Section = 'overview' | 'tasks' | 'inbox' | 'analytics';
-
-
 interface LayoutProps {
     children: ReactNode
 }
@@ -14,26 +10,18 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
 
     const [isSidebarOpen, setSidebarOpen] = useState(true);
-    const [ActiveSection, setActiveSection] = useState<Section>('overview');
 
     return (
         <div className="flex min-h-screen">
             {isSidebarOpen &&
-                (<SideBar setSidebarOpen={setSidebarOpen}
-                    ActiveSection={ActiveSection}
-                    setActiveSection={setActiveSection}
-                />)}
+                (<SideBar setSidebarOpen={setSidebarOpen} />)}
 
             <div className="flex-1 flex flex-col">
                 <NavBar isSidebarOpen={isSidebarOpen} setSidebarOpen={setSidebarOpen} />
 
-                <main className="p-5">
-                    {ActiveSection == 'overview' && <div> OverView</div>}
-                    {ActiveSection == 'tasks' && <div>Tasks</div>}
-                    {ActiveSection == 'inbox' && <div>Inbox</div>}
-                    {ActiveSection == 'analytics' && <div>Analytics</div>}
-
-                </main>
+                <div className="p-4">
+                {children}
+                </div>
             </div>
         </div>
 
---
+++ src/app/dashboard/overview/page.tsx
@@ -0,0 +1,5 @@
+export default function OverView(){
+    return(
+        <div>this is Overview </div>
+    )
+}
