2d79e4076e9e68c8c1b2095ad89c5e5dbf71d645
jtn11 <jatinsainii007@gmail.com>
2026-07-01 22:00:42 +0530

    successfull login redirect to overview pg

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,18 +1,5 @@
-"use client";
-
-import { Square3Stack3DIcon } from "@heroicons/react/24/solid";
+import { redirect } from "next/navigation";
 
 export default function DashboardPage() {
-  return (
-    <div className="w-full flex flex-col">
-      <div className="p-5 flex justify-center items-center mt-50 mr-35">
-        <Square3Stack3DIcon className="w-20 h-20 text-gray-500" />
-      </div>
-      <div>
-        <span className="p-5 flex justify-center items-center mr-35">
-          Assign Task
-        </span>
-      </div>
-    </div>
-  );
+  redirect("/dashboard/overview");
 }
